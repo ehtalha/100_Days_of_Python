f6b0f390fb70571bc0840361a800c64eb4449629
Emdadul Haque Talha <168511664+ehtalha@users.noreply.github.com>
2026-04-28 22:15:19 +0600

    Add files via upload

---
+++ Chpter_Six/Practice2.py
@@ -0,0 +1,18 @@
+# Even Odd
+num = 7
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# odd
+
+num = int(input("Enter a number : "))
+print(f"number is {num}")
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+# Enter a number : 10
+# number is 10
+# Even
