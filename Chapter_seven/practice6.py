846173951651ffc80726494e85fa73f1555918eb
Emdadul Haque Talha <168511664+ehtalha@users.noreply.github.com>
2026-04-30 15:36:03 +0600

    Create practice6.py

---
+++ Chapter_seven/practice6.py
@@ -0,0 +1,14 @@
+# Square pattern
+n = int(input("Enter a number : "))
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print() # Moves to the next line
+
+'''
+Enter a number: 4
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+'''
