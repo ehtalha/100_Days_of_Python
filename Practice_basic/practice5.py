75134f1e3940c1c262f1ddc3290b85f0224e7c63
Emdadul Haque Talha <168511664+ehtalha@users.noreply.github.com>
2026-05-01 23:22:09 +0600

    Create practice5.py

---
+++ Practice_basic/practice5.py
@@ -0,0 +1,10 @@
+# Fizzbuzz logic
+for i in range(1, 21): # Shortened to 20 for brevity
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
