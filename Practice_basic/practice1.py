f6e4884f4ee4e3073225517e489fe9b0179da4be
Emdadul Haque Talha <168511664+ehtalha@users.noreply.github.com>
2026-05-01 23:17:40 +0600

    Create practice1.py

---
+++ Practice_basic/practice1.py
@@ -0,0 +1,8 @@
+# Diamond Pattern
+n = 5
+# Top half
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+# Bottom half
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
